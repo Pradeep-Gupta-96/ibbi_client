285681dfe10e9db51686ce5af257564b2518c973
Pradeep Kumar Gupta <120917855+Pradeep-Gupta-96@users.noreply.github.com>
2023-09-05 18:11:21 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import Navbar from "./Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import PublicAnnouncement from "./component/Public_announcement";
+
 
 function App() {
   return (
-    <div className="App">
-      containt is comming soon...!
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<PublicAnnouncement />} />
+        </Routes>
+
+      </Router>
+    </>
   );
 }
 
---
+++ src/component/Public_announcement.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Public_announcement'
+
+const Public_announcement = () => {
+  return (
+    <div>Public_announcement</div>
+  )
+}
+
+export default Public_announcement
