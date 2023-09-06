5cc725afa4e3fdc1c945259a646baa8d3c31847a
Areness Consulting <Pradeep-Gupta-96>
2023-09-06 15:49:41 +0530

    code updated

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,253 +1,57 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { styled, alpha } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
-import Divider from '@mui/material/Divider';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import './Navbar.css'
-import { useNavigate } from "react-router-dom"
-import { Drawer, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-const drawerWidth = 240;
-
-const StyledMenu = styled((props) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-  },
-}));
-
-
-function Navbar(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const open = Boolean(anchorEl);
-  const naviagte = useNavigate()
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const onClickForAboutUs = () => {
-    naviagte('/about')
-  }
-  const onClickHome = () => {
-    naviagte('/')
-  }
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-
-      <Typography variant="h6" sx={{ my: 2 }}>
-        IBC Support
-      </Typography>
-
-      <Divider />
-
-      <Button onClick={onClickForAboutUs}>
-        AboutUs
-      </Button>
-      <Button
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        variant="contained"
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        Options
-      </Button>
-      <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose} disableRipple>
-          <EditIcon />
-          Edit
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <FileCopyIcon />
-          Duplicate
-        </MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem onClick={handleClose} disableRipple>
-          <ArchiveIcon />
-          Archive
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <MoreHorizIcon />
-          More
-        </MenuItem>
-      </StyledMenu>
-
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+import React from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+import banner from './img/banner_1.png'
+const Navbar = () => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
 
-      <AppBar component="nav">
-        <div className='bound'>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-              onClick={onClickHome}
-            >
-              IBC Support
-            </Typography>
-
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-              <Button onClick={onClickForAboutUs}>
-                AboutUs
-              </Button>
-              <Button
-                id="demo-customized-button"
-                aria-controls={open ? 'demo-customized-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                variant="contained"
-                disableElevation
-                onClick={handleClick}
-                endIcon={<KeyboardArrowDownIcon />}
-              >
-                Options
-              </Button>
-              <StyledMenu
-                id="demo-customized-menu"
-                MenuListProps={{
-                  'aria-labelledby': 'demo-customized-button',
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose} disableRipple>
-                  <EditIcon />
-                  Edit
-                </MenuItem>
-                <MenuItem onClick={handleClose} disableRipple>
-                  <FileCopyIcon />
-                  Duplicate
-                </MenuItem>
-                <Divider sx={{ my: 0.5 }} />
-                <MenuItem onClick={handleClose} disableRipple>
-                  <ArchiveIcon />
-                  Archive
-                </MenuItem>
-                <MenuItem onClick={handleClose} disableRipple>
-                  <MoreHorizIcon />
-                  More
-                </MenuItem>
-              </StyledMenu>
-
-            </Box>
-
-          </Toolbar>
+    <>
+      <div className="header">
+        <div className="bound">
+          <div className="topsec">
+            <div className="logo">IBC Support</div>
+            <div className="menu">
+              <Link to="javascript-void()" className='toggle-menu' style={{display:"none"}}>Menu</Link>
+              <ul>
+                <li className="nav-item parent"><Link className="nav-Link" to="#">Home</Link>
+                  <ul>
+                    <li><Link to="#">About Us</Link></li>
+                  </ul>
+                </li>
+                <li className="nav-item parent"><Link className="nav-Link" to="#">Services</Link>
+                  <ul>
+                    <li><Link to="#">For Financial Creditor</Link></li>
+                    <li><Link to="#">For Operational Creditor</Link></li>
+                    <li><Link to="#">For Resolution Applicant</Link></li>
+                  </ul>
+                </li>
+                <li className="nav-item"><Link className="nav-Link" to="#">Orders</Link></li>
+                <li className="nav-item"><Link className="nav-Link" to="#">Public Announcements</Link></li>
+                <li className="nav-item"><Link className="nav-Link" to="#">Blog</Link></li>
+                <li className="nav-item"><Link className="nav-Link" to="#">Contacts</Link></li>
+                <li className="nav-item get-free"><Link className="nav-Link" to="#">Get Free Consultation</Link></li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-
-    </Box>
-  );
+      </div>
+      <div className="banner">
+        <div className="bound">
+          <div className="main-banner"><img src={banner} alt="banner" /></div>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <Link className="dropdown-item" to="#">Action</Link>
+          <Link className="dropdown-item" to="#">Another action</Link>
+          <Link className="dropdown-item" to="#">Something else here</Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
-Navbar.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
-
-
-export default Navbar;
-
+export default Navbar
