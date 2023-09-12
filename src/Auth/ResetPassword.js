e1b21c12c48e6b1633e67994abee477cb506d5f0
Areness Consulting <Pradeep-Gupta-96>
2023-09-12 12:52:44 +0530

    code updated

--- src/Auth/ResetPassword.js
+++ src/Auth/ResetPassword.js
@@ -19,7 +19,9 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
-
+const AnimatedGridItem = styled(Typography)`
+font-family: 'Montserrat', sans-serif;
+`;
 
 function ResetPassword() {
   const [visivility, setVisivility] = React.useState(false)
@@ -33,26 +35,21 @@ function ResetPassword() {
   }
 
   return (
-    <>
-      <Grid container className='container' spacing={0}>
-        <Grid item className='container1' xs={6} md={8}>
-        </Grid>
-        <Grid item className='container2' xs={6} md={4}>
-        </Grid>
-        <Grid item className='login-container' xs={4}>
-          <Item>
-            <Avatar sx={{ mx: 'auto', mt: 5, bgcolor: 'secondary.main' }}>
+    <><div className="auth-content">
+    <div className="bound">
+        <Grid container>
+            <Grid item xs={12} md={4}></Grid>
+            <Grid className='p-50' item xs={12} md={4}>
+                <Item className='auth-form'>
+                <Avatar sx={{ mx: 'auto', mt: 5, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar>
-            <Typography component="h1" variant="h5" sx={{ my: 1, textAlign: "center" }}>
-              Recqarz
-            </Typography>
-            <Typography component="h1" variant="h6" sx={{ my: 1, textAlign: "center" }}>
-              Wellcome to Recqarz <DoneAllIcon />
-            </Typography>
-            <Typography component="h1" variant="subtitle1" sx={{ my: 1, textAlign: "center" }}>
-              Please sign in to your account and start analyzing.
-            </Typography>
+           <AnimatedGridItem component="h1" variant="h6" sx={{ my: 1, textAlign: "center" }}>
+              Wellcome to IBC Support <DoneAllIcon />
+            </AnimatedGridItem>
+            <AnimatedGridItem component="h1" variant="subtitle1" sx={{ my: 1, textAlign: "center" }}>
+              Please Forgot your Password and start analyzing.
+            </AnimatedGridItem>
             <TextField
               margin="normal"
               required
@@ -81,18 +78,20 @@ function ResetPassword() {
               id="password"
               autoComplete="current-password"
             />
-            <Button
+            <Button className='submit-btn'
               type="submit"
               variant="contained"
             >
-              Sign In
+              Forgot Password
             </Button>
             <Divider />
             <Divider />
-          </Item>
+                </Item>
+            </Grid>
+            <Grid item xs={12} md={4}></Grid>
         </Grid>
-
-      </Grid>
+    </div>
+</div>
     </>
   )
 }
--- src/Auth/Signin.js
+++ src/Auth/Signin.js
@@ -11,90 +11,92 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { Divider, Grid, IconButton, InputAdornment } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
 }));
+
+const AnimatedGridItem = styled(Typography)`
+font-family: 'Montserrat', sans-serif;
+`;
+
 
 
 function Signin() {
-  const [visivility, setVisivility] = React.useState(false)
+    const [visivility, setVisivility] = React.useState(false)
 
-  const changeicons = () => {
-    if (visivility === false) {
-      setVisivility(true)
-    } else {
-      setVisivility(false)
+    const changeicons = () => {
+        if (visivility === false) {
+            setVisivility(true)
+        } else {
+            setVisivility(false)
+        }
     }
-  }
 
-  return (
-    <>
-      <Grid container className='container' spacing={0}>
-        <Grid item className='container1' xs={6} md={8}>
-        </Grid>
-        <Grid item className='container2' xs={6} md={4}>
-        </Grid>
-        <Grid item className='login-container' xs={4}>
-          <Item>
-            <Avatar sx={{ mx: 'auto', mt: 5, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5" sx={{ my: 1, textAlign: "center" }}>
-              Recqarz
-            </Typography>
-            <Typography component="h1" variant="h6" sx={{ my: 1, textAlign: "center" }}>
-              Wellcome to Recqarz <DoneAllIcon />
-            </Typography>
-            <Typography component="h1" variant="subtitle1" sx={{ my: 1, textAlign: "center" }}>
-              Please sign in to your account and start analyzing.
-            </Typography>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="username"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type={visivility ? "text" : "password"}
-              InputProps={{
-                endAdornment:
-                  <InputAdornment position='end'>
-                    <IconButton onClick={changeicons}>
-                      {visivility ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                    </IconButton>
-                  </InputAdornment>
-              }}
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              variant="contained"
-            >
-              Sign In
-            </Button>
-            <Divider />
-            <Divider />
-          </Item>
-        </Grid>
+    return (
+        <>
+            <div className="auth-content">
+                <div className="bound">
+                    <Grid container>
+                        <Grid item xs={12} md={4}></Grid>
+                        <Grid className='p-50' item xs={12} md={4}>
+                            <Item className='auth-form'>
+                                <Avatar sx={{ mx: 'auto', mt: 5, bgcolor: 'secondary.main' }}>
+                                    <LockOutlinedIcon />
+                                </Avatar>
+                                <AnimatedGridItem component="h1" variant="h6" sx={{ my: 1, textAlign: "center" }}>
+                                    Wellcome to IBC Support <DoneAllIcon />
+                                </AnimatedGridItem>
+                                <AnimatedGridItem component="h1" variant="subtitle1" sx={{ my: 1, textAlign: "center" }}>
+                                    Please sign in to your account and start analyzing.
+                                </AnimatedGridItem>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="username"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type={visivility ? "text" : "password"}
+                                    InputProps={{
+                                        endAdornment:
+                                            <InputAdornment position='end'>
+                                                <IconButton onClick={changeicons}>
+                                                    {visivility ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                    }}
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                                <div className="links"> <Link to="/resetpassword">Forgot Password</Link> <Link to="/signup">Signup</Link></div>
+                                <Button className='submit-btn' type="submit" variant="contained" > Sign In </Button>
+                                
+                                <Divider />
+                                <Divider />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} md={4}></Grid>
+                    </Grid>
+                </div>
+            </div>
 
-      </Grid>
-    </>
-  )
+        </>
+    )
 }
 
 export default Signin
